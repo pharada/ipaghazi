43b3f2ede69529dad6299d5717d39c01efec0915
Samuel Harada <sam@pharada.org>
2016-08-31 17:41:24 -0400

    Nuclear launch codes

--- config.sh
+++
@@ -1,7 +0,0 @@
-export \
-    IPAGHAZI_ROOT_KEY=herpderp \
-    IPAGHAZI_BASEURL=http://localhost:8088/ipaghazi \
-    IPAGHAZI_MONGODB=mongodb://localhost/ipaghazi \
-    IPAGHAZI_ROOT_USER=root \
-    IPAGHAZI_ANON_PERMS=browse-app \
-    IPAGHAZI_METHODS=file
